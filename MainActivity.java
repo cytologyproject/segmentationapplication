1dba26fc2b1df161ce0a92b931c0f01ec383df57
ROBOGENN <39436055+ROBOGENN@users.noreply.github.com>
2019-03-03 21:12:39 +0400

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,43 +1,89 @@
-package com.example.camerasample;
-import androidx.appcompat.app.AppCompatActivity;
+package com.example.camerasample2;
 
+import android.Manifest;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.os.Environment.getExternalStoragePublicDirectory;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView mimageView;
-    private static final  int REQUEST_IMAGE_CAPTURE = 101;
+    Button btnTakePic;
+    ImageView imageView;
+    String pathToFile;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mimageView = findViewById(R.id.ImageView);
-    }
-
-    public void takePicture(View view)
-    {
-        Intent imageTakeIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-        if(imageTakeIntent.resolveActivity(getPackageManager())!=null)
-        {
-            startActivityForResult(imageTakeIntent, REQUEST_IMAGE_CAPTURE);
+        btnTakePic = findViewById(R.id.btnTakePic);
+        if (Build.VERSION.SDK_INT >= 23) {
+            requestPermissions(new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 2);
         }
+        btnTakePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchPictureTakeAction();
+            }
+        });
+        imageView = findViewById(R.id.image);
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
-        if(requestCode==REQUEST_IMAGE_CAPTURE && resultCode==RESULT_OK)
-        {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            mimageView.setImageBitmap(imageBitmap);
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 1) {
+                Bitmap bitmap = BitmapFactory.decodeFile(pathToFile);
+                imageView.setImageBitmap(bitmap);
+            }
         }
+    }
+
+    private void dispatchPictureTakeAction() {
+        Intent takePic = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePic.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            photoFile = createPhotoFile();
+
+            if (photoFile != null) {
+                pathToFile = photoFile.getAbsolutePath();
+                Uri photoURI = FileProvider.getUriForFile(MainActivity.this, "com.example.camerasample2.fileprovider", photoFile);
+                takePic.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePic, 1);
+
+            }
+        }
+    }
+
+    private File createPhotoFile() {
+        String name = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File storageDir = getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File image = null;
+        try {
+            image = File.createTempFile(name, ".jpg", storageDir);
+        }   catch (IOException e) {
+            Log.d("mylog", "Excep : " + e.toString());
+        }
+        return image;
     }
 }
