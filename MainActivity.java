6948d24fe1ec4c66539fcedb8a106387be991722
laisbel <47768705+laisbel@users.noreply.github.com>
2019-03-08 19:34:05 +0500

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -86,4 +86,69 @@ public class MainActivity extends AppCompatActivity {
         }
         return image;
     }
+    //инверсия цвета
+    public Bitmap invertImage(Bitmap src) {
+    // create new bitmap with the same attributes(width,height)
+    //as source bitmap
+        Bitmap bmOut = Bitmap.createBitmap(src.getWidth(), src.getHeight(), src.getConfig());
+        // color info
+        int R, G, B;
+        int pixelColor;
+        // image size
+        int height = src.getHeight();
+        int width = src.getWidth();
+
+        // scan through every pixel
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                pixelColor = src.getPixel(x, y);
+                // inverting byte for each R/G/B channel
+                R = 255 - Color.red(pixelColor);
+                G = 255 - Color.green(pixelColor);
+                B = 255 - Color.blue(pixelColor);
+                // set newly-inverted pixel to output image
+                bmOut.setPixel(x, y, Color.rgb(R, G, B));
+            }
+        }
+        // return final bitmap
+        return bmOut;
+    }
+    //перевод в оттенки серого
+    public static Bitmap doGreyscale(Bitmap src) {
+        // constant factors
+        final double GS_RED = 0.299;
+        final double GS_GREEN = 0.587;
+        final double GS_BLUE = 0.114;
+
+        // create output bitmap
+        Bitmap bmOut = Bitmap.createBitmap(src.getWidth(), src.getHeight(), src.getConfig());
+        // pixel information
+        int R, G, B;
+        int pixel;
+
+        // get image size
+        int width = src.getWidth();
+        int height = src.getHeight();
+
+        // scan through every single pixel
+        for(int x = 0; x < width; ++x) {
+            for(int y = 0; y < height; ++y) {
+                // get one pixel color
+                pixel = src.getPixel(x, y);
+                // retrieve color of all channels
+                R = Color.red(pixel);
+                G = Color.green(pixel);
+                B = Color.blue(pixel);
+                // take conversion up to one single value
+                R = G = B = (int)(GS_RED * R + GS_GREEN * G + GS_BLUE * B);
+                // set new pixel color to output bitmap
+                bmOut.setPixel(x, y, Color.rgb(R, G, B));
+            }
+        }
+
+        // return final image
+        return bmOut;
+    }
 }
